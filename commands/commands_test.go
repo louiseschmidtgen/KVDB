b71b31247dc387f42e134b1d60e39a36e25d61f2
louiseschmidtgen <louise.schmidtgen@gmail.com>
2023-08-23 16:57:23 -0600

    add commands test

---
+++ commands/commands_test.go
@@ -0,0 +1,75 @@
+package commands_test
+
+import (
+	"bytes"
+	"log"
+	"os"
+	"testing"
+
+	"github.com/louiseschmidtgen/KVDB/commands"
+	"github.com/louiseschmidtgen/KVDB/database"
+	"github.com/spf13/cobra"
+	"github.com/stretchr/testify/require"
+)
+
+func TestSetGetCommandWithFlag(t *testing.T) {
+	// Create and initialize a temporary test database
+	db, err := database.InitKeyValueDB("test.db")
+	require.NoError(t, err)
+	defer func() {
+		db.Close()
+		os.Remove("test3.db")
+	}()
+
+	cmd := &cobra.Command{
+		Use:   "set",
+		Short: "Set a key-value pair",
+		Run: func(cmd *cobra.Command, args []string) {
+			commands.Set(cmd, args)
+		},
+	}
+
+	// Set up test arguments for SetCommand
+	key := "greeting"
+	value := "hi"
+	kv := []string{key, value}
+	cmd.SetArgs(kv)
+
+	// Set the filename flag for testing
+	filenameFlagName := "database"
+	cmd.Flags().String(filenameFlagName, "test3.db", "Database filename for testing")
+
+	// Execute the SetCommand with the filename flag
+	err = cmd.Execute()
+	require.NoError(t, err)
+
+	// Initialize the GetCommand
+	cmd = &cobra.Command{
+		Use:   "set",
+		Short: "Set a key-value pair",
+		Run: func(cmd *cobra.Command, args []string) {
+			commands.Get(cmd, args)
+		},
+	}
+
+	// Set up test arguments for GetCommand
+	k := []string{key}
+	cmd.SetArgs(k)
+
+	// Create a buffer to capture stdout
+	var stdoutBuffer bytes.Buffer
+
+	// Redirect stdout to the buffer
+	log.SetOutput(&stdoutBuffer)
+
+	// Set the filename flag for testing in GetCommand
+	cmd.Flags().String(filenameFlagName, "test3.db", "Database filename for testing")
+
+	// Execute the GetCommand with the filename flag
+	err = cmd.Execute()
+	require.NoError(t, err)
+
+	// Check the content of the outputBuffer for our key and value
+	expectedOutput := "Value for 'greeting': hi"
+	require.Contains(t, stdoutBuffer.String(), expectedOutput)
+}
